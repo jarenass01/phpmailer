d0741dd9cfb9bed629c2251f246fbef530ea270b
Juan Fernando Arenas Silva <juanfernandomx@gamil.com>
2014-08-21 15:29:53 -0500

    Carga inicial de archivos

---
+++ index.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>PHPMailer - GMail SMTP test</title>
+</head>
+<body>
+<?php
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+
+require 'mail/PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+//Set the hostname of the mail server
+$mail->Host = '';
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "example@example.com";
+//Password to use for SMTP authentication
+$mail->Password = "";
+//Set who the message is to be sent from
+$mail->setFrom('example@example.com', 'Example NAme');
+//Set an alternative reply-to address
+$mail->addReplyTo('example@example.com', 'Solar');
+
+$destiny= array('','');
+
+foreach ($destiny as $key) {
+    $mail->addBCC($key);
+}
+//Set who the message is to be sent to
+//$mail->addAddress();
+//Set the subject line
+$mail->Subject = 'Pruebas de envío de mail';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML("<h1>Prueba de arrays</h1>");
+//Replace the plain text body with one created manually
+$mail->AltBody = 'Que sera esto ome';
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.gif');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+?>
+</body>
+</html>
